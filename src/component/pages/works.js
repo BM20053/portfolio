ae76caac2cd9b9d4b6c2b6af7a50f9f2cc63b84c
BM20053 <shigeharu0310@outlook.jp>
2022-08-11 19:32:04 +0900

    works書き込み

--- src/component/pages/works.js
+++ src/component/pages/works.js
@@ -4,8 +4,24 @@ import './page.css';
 const Works = () => {
     return(
         <div className="works" id='works'>
-            <h1>Works</h1>
-            <h2>大学４年間でやったこと</h2>
+            <div className='title'>
+                Works
+            </div>
+                <div className='achievement'>
+                    <h2>【実績】</h2>
+                    <li>基本情報技術者試験合格！</li>
+                    <li>広島工業大学学生自治会文化局所属/会計</li>
+                    <li>１学年の時、課外活動、学内活動の参加量が学科で１番になる</li>
+                    <li>プログラミングやCG、３Dモデリングを勉強するサークルの副代表になる</li>
+                    <li>大学主催、小学生対象の体験型講座にスタッフ参加</li>
+                    <li>広島銀行主催のビジネスコンテストにチームで参加</li>
+                    <li>ビジネスコンテストへの参加、アイディアソン、技術力の向上を目的とした「起業愛好会」を設立</li>
+                </div>
+                <div className='message'>
+                    <p>どんどん改善、更新していくので、またあそびに来てね！</p>
+                </div>
+
+
             </div>
     )
 }
