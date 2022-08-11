668b60a8d58f39a1491941f43f88ece4f319759f
BM20053 <shigeharu0310@outlook.jp>
2022-08-11 16:16:14 +0900

    写真追加

--- src/component/pages/home.js
+++ src/component/pages/home.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './page.css';
+import Image from'./img/img1.jpg'
 
-const Home = () => {
-    return(
-        <div className="home">
-            <div className = "title">
-                Welcome!
+export default class Home extends React.Component {
+    render() {
+        return(
+            <div className="home" id='home'>
+                <div className = "title">
+                    Welcome!
+                    </div>
+                <div className='sabtitle'>
+                    ShigemotoLab
                 </div>
-            <div className='sabtitle'>
-                ShigemotoLab
-                </div>
+                <input type="image" img src={Image} alt="Image" />
             </div>
 
-    )
+        )
+    }
+    
 }
-export default Home;
+// export default Home;
+
+// export const IconComponent = () =>{
+    // return <image src={Image}  alt="Image" />
+//   }
+//   export default IconComponent
